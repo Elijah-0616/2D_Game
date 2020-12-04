3c2fdedc6cf7f83017b9dfe4e2c9618109b8d269
Elijah-0616 <60286615+Elijah-0616@users.noreply.github.com>
2020-12-04 10:49:09 +0800

    Initial commit

---
+++ Assets/Scripts/MoveAndContact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class MoveAndContact : MonoBehaviour {
+    //定义针发射速度
+    private float speed = 20f;
+
+    /*
+     * 检测碰撞
+     */
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.name =="Sphere")
+        {
+            speed = 0;
+        }
+    }
+    // Update is called once per frame
+    private void Update() {
+        if (Input.GetButton("Fire1")) {
+            /*
+             * 发射针，冻结X,Z轴速度
+             */
+            var movement = new Vector3(0.0f, speed, 0.0f);
+            GetComponent<Rigidbody>().velocity = movement;
+            Debug.Log("当前速度=" + GetComponent<Rigidbody>().velocity.y);
+        }
+    }
+    
+   
+
+    
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //物体自转代码
+        transform.Rotate (Vector3.forward,45*Time.deltaTime,Space.Self);    
+    }
+}
