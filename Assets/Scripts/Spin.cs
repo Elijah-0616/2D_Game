3f1c2228525ce5710c1a41ccb55168f59e84118f
Elijah-0616 <60286615+Elijah-0616@users.noreply.github.com>
2020-12-16 17:11:55 +0800

    2020.12.16 修复if语句执行两次的bug，优化体验

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -2,12 +2,26 @@
 
 public class Spin : MonoBehaviour
 {
-   
-
+    public Rigidbody2D rb;
+    public float speed = 100;
+    private GameController _gameController;
+    
+    private void Start()
+    {
+        var gameControllerObject = GameObject.FindWithTag("MainCamera");
+        _gameController = gameControllerObject.GetComponent<GameController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     private void Update()
     {
-        //物体自转代码
-        transform.Rotate (Vector3.forward,45*Time.deltaTime,Space.Self);    
+        //如果游戏状态为结束则不再自转
+        if (! _gameController.overOnOff)
+        {
+            //球体自转
+            transform.Rotate (0,
+                0, 
+                speed * Time.deltaTime);
+        }    
     }
 }
